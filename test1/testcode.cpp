8f49e330337fcdd354194c60a9afb4b40b04c310
Your Name <you@example.com>
2020-11-11 00:56:04 -0500

    it pulled and then add a new line in testcode.cpp

--- test1/testcode.cpp
+++ test1/testcode.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main() {
 	cout << "Hello word!!" << endl;
 	cout << "Hello from GitHub web!!" << endl;
+	cout << "But now I am in brach master again" << endl;
+
 
 	return 0;
 }
